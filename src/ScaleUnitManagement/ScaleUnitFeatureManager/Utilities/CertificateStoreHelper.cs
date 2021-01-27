7a7aad7ed3794d8a76e8f32299bad1a80e99cd6c
Alexandros Kalomoiros <alkalomo@microsoft.com>
2021-01-27 12:39:49 +0100

    fix certificate creation script

--- src/ScaleUnitManagement/ScaleUnitFeatureManager/Utilities/CertificateStoreHelper.cs
+++ src/ScaleUnitManagement/ScaleUnitFeatureManager/Utilities/CertificateStoreHelper.cs
@@ -62,8 +62,9 @@ namespace ScaleUnitManagement.ScaleUnitFeatureManager.Utilities
                 + " -HashAlgorithm " + CommandExecutor.Quotes + "SHA256" + CommandExecutor.Quotes
                 + " -CertStoreLocation " + CommandExecutor.Quotes + @"Cert:\LocalMachine\My" + CommandExecutor.Quotes
                 + " -KeyUsage KeyEncipherment -FriendlyName " + CommandExecutor.Quotes + "HTTPS development certificate" + CommandExecutor.Quotes
-                + " -TextExtension @(" + CommandExecutor.Quotes + "2.5.29.19={critical}{text}" + CommandExecutor.Quotes + "," + CommandExecutor.Quotes + "2.5.29.37={critical}{text}" + CommandExecutor.Quotes
-                + "," + CommandExecutor.Quotes + "2.5.29.37={critical}{text}1.3.6.1.5.5.7.3.1" + CommandExecutor.Quotes + "," + CommandExecutor.Quotes + "2.5.29.17={critical}{text}DNS=" + certificateSubject + CommandExecutor.Quotes + ")";
+                + " -TextExtension @(" + CommandExecutor.Quotes + "2.5.29.19={critical}{text}" + CommandExecutor.Quotes
+                + "," + CommandExecutor.Quotes + "2.5.29.37={critical}{text}1.3.6.1.5.5.7.3.1" + CommandExecutor.Quotes
+                + "," + CommandExecutor.Quotes + "2.5.29.17={critical}{text}DNS=" + certificateSubject + CommandExecutor.Quotes + ")";
 
             CommandExecutor ce = new CommandExecutor();
             ce.RunCommand(cmd);
