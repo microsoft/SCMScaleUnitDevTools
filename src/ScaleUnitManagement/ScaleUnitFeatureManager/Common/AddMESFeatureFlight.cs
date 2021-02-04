596798fe53adf5b54b4327a099eedee9b40a4f9f
Andreas Hassing <anniels@microsoft.com>
2021-02-04 16:55:43 +0100

    Ensure MES flight is inserted with Hub ID to avoid conflicts

--- src/ScaleUnitManagement/ScaleUnitFeatureManager/Common/AddMESFeatureFlight.cs
+++ src/ScaleUnitManagement/ScaleUnitFeatureManager/Common/AddMESFeatureFlight.cs
@@ -14,7 +14,8 @@ namespace ScaleUnitManagement.ScaleUnitFeatureManager.Common
 
         public float Priority()
         {
-            // before starting services again
+            // after DbSync (we need the triggers and SYSSCALEUNITID columns),
+            // before starting services again,
             return 3.9F;
         }
 
@@ -24,6 +25,9 @@ namespace ScaleUnitManagement.ScaleUnitFeatureManager.Common
 
             string sqlQuery = $@"
 USE {scaleUnit.AxDbName};
+
+-- insert row under Hub scale unit id to avoid merge conflicts later, as the table is synced Hub->Scale Unit
+EXEC sys.sp_set_session_context @key = N'ActiveScaleUnitId', @value = '@@';
 
 IF NOT EXISTS (SELECT TOP 1 1 FROM SysFlighting WHERE FlightName = '{MESFlightName}')
     INSERT INTO SysFlighting (FlightName, Enabled, FlightServiceId) VALUES ('{MESFlightName}', 1, 12719367);
