6b261c0adb7d9ded2d4426a8116b8b02774e24dc
Christian Abeln <christian.abeln@microsoft.com>
2021-02-05 15:53:37 +0100

    Admin required check

--- src/CLI/Program.cs
+++ src/CLI/Program.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using CLIFramework;
 
@@ -6,17 +8,40 @@ namespace CLI
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static int Main(string[] args)
         {
-            CLIOption enableScaleUnitFeatureOption = new CLIOption() { Name = "Initialize the hybrid topology", Command = EnableScaleUnitFeature.SelectScaleUnit };
-            CLIOption configureEnvironmentOption = new CLIOption() { Name = "Prepare environments for workload installation", Command = ConfigureEnvironment.Show };
-            CLIOption installWorkloadsOption = new CLIOption() { Name = "Install workloads", Command = InstallWorkloads.Show };
-            CLIOption workloadsInstallationStatusOption = new CLIOption() { Name = "Show workloads installation status", Command = WorkloadsInstallationStatus.Show };
+            if (!HasAdministratorPrivileges())
+            {
+                Console.Error.WriteLine("Access Denied!\r\n" +
+                    "This tool must be run with administrator permissions.\r\n" +
+                    "Please start the tool in an administrator command prompt.\r\n\r\n" + 
+                    "Press any key to exit. . .");
+                Console.ReadKey();
+                return 740; // ERROR_ELEVATION_REQUIRED
+            }
+
+            MainAsync(args).GetAwaiter().GetResult();
+            return 0;
+        }
+
+        public static async Task MainAsync(string[] args)
+        {
+            var enableScaleUnitFeatureOption = new CLIOption() { Name = "Initialize the hybrid topology", Command = EnableScaleUnitFeature.SelectScaleUnit };
+            var configureEnvironmentOption = new CLIOption() { Name = "Prepare environments for workload installation", Command = ConfigureEnvironment.Show };
+            var installWorkloadsOption = new CLIOption() { Name = "Install workloads", Command = InstallWorkloads.Show };
+            var workloadsInstallationStatusOption = new CLIOption() { Name = "Show workloads installation status", Command = WorkloadsInstallationStatus.Show };
 
             var options = new List<CLIOption>() { enableScaleUnitFeatureOption, configureEnvironmentOption, installWorkloadsOption, workloadsInstallationStatusOption };
 
-            CLIScreen screen = new CLIScreen(options, "Home", "Please select the operation you want to perform:\n", "\nOperation to perform: ");
+            var screen = new CLIScreen(options, "Home", "Please select the operation you want to perform:\n", "\nOperation to perform: ");
             await CLIMenu.ShowScreen(screen);
+        }
+
+        private static bool HasAdministratorPrivileges()
+        {
+            var id = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(id);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
         }
     }
 }
