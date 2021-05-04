5af4d3a35cd7c4bcf3a5b9b10cb6049873019457
Andreas Hassing <anniels@microsoft.com>
2021-05-04 16:06:21 +0200

    CLRify IIS and Service Operations

---
+++ src/ScaleUnitManagement/ScaleUnitFeatureManager/Utilities/ServiceHelper.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Linq;
+using System.ServiceProcess;
+
+namespace ScaleUnitManagement.ScaleUnitFeatureManager.Utilities
+{
+    public static class ServiceHelper
+    {
+        public static void StartService(string serviceName, TimeSpan timeout) =>
+            RunServiceAction(serviceName, svc => {
+                Console.WriteLine($"Starting service {serviceName}..");
+
+                svc.Start();
+                svc.WaitForStatus(ServiceControllerStatus.Running, timeout);
+
+                Console.WriteLine($"Successfully started service {serviceName}");
+            });
+
+        public static void StopService(string serviceName, TimeSpan timeout) =>
+            RunServiceAction(serviceName, svc => {
+                Console.WriteLine($"Stopping service {serviceName}..");
+
+                svc.Stop();
+                svc.WaitForStatus(ServiceControllerStatus.Stopped, timeout);
+
+                Console.WriteLine($"Successfully stopped service {serviceName}");
+            });
+
+        private static void RunServiceAction(string serviceName, Action<ServiceController> action)
+        {
+            if (!ServiceExists(serviceName))
+            {
+                return;
+            }
+
+            using (ServiceController service = new ServiceController(serviceName))
+            {
+                action(service);
+            }
+        }
+
+        private static bool ServiceExists(string serviceName)
+        {
+            return ServiceController.GetServices().FirstOrDefault(s => s.ServiceName == serviceName) != null;
+        }
+    }
+}
