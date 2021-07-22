95573f58cda7dae6b296ebab3b515e17331cc5f2
Antonstockmarr <43210147+Antonstockmarr@users.noreply.github.com>
2021-07-22 14:53:41 +0200

    Checking if the scaleUnitId is valid

--- src/ScaleUnitManagement/Utilities/Config.cs
+++ src/ScaleUnitManagement/Utilities/Config.cs
@@ -149,6 +149,10 @@ namespace ScaleUnitManagement.Utilities
                 ValidateValue("AppSecret", scaleUnit.AuthConfiguration.AppSecret);
                 ValidateValue("Authority", scaleUnit.AuthConfiguration.Authority);
 
+                int integralScaleUnitId = ScaleUnitMnemonicCalculator.ToIntegralValue(scaleUnit.ScaleUnitId);
+                if (!scaleUnit.IsHub() && (integralScaleUnitId < 1 || 1000 <= integralScaleUnitId))
+                    Console.Error.WriteLine("ScaleUnitId is not in the mnemonic range (between 0 and 1000)");
+
                 if (Config.ScaleUnitInstances().Where(s => s.ScaleUnitId == scaleUnit.ScaleUnitId).Count() != 1)
                     Console.Error.WriteLine("ScaleUnitId is not unique");
 
