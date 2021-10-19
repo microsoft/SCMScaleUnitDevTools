dacb5f15729487f1423662382c9f5f5231d5ec79
Antonstockmarr <43210147+Antonstockmarr@users.noreply.github.com>
2021-10-19 05:33:55 -0700

    Update MaxBatchSessions during initialization
    Setting maxBatchSessions to 16 avoids getting stuck during workload movements. Having more than 16 can cause performance issues.
    
    resolves #104

---
+++ src/ScaleUnitManagement/ScaleUnitFeatureManager/Common/UpdateMaxBatchSessions.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using ScaleUnitManagement.ScaleUnitFeatureManager.Utilities;
+
+namespace ScaleUnitManagement.ScaleUnitFeatureManager.Common
+{
+    public class UpdateMaxBatchSessions : ICommonStep
+    {
+        public string Label()
+        {
+            return "Update max batch sessions";
+        }
+
+        public float Priority()
+        {
+            return 3.5F;
+        }
+
+        public Task Run()
+        {
+            int maxBatchSessions = 16;
+            string query = $"UPDATE t1 " +
+                $"SET t1.MAXBATCHSESSIONS = {maxBatchSessions} " +
+                $"FROM BATCHSERVERCONFIG t1 " +
+                $"WHERE t1.MAXBATCHSESSIONS < {maxBatchSessions}";
+
+            var sqlQueryExecutor = new SqlQueryExecutor();
+            sqlQueryExecutor.Execute(query);
+
+            return Task.CompletedTask;
+        }
+    }
+}
