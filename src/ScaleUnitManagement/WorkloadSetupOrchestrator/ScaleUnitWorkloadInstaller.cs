5870f9792e5689af312cb52d3488dbe178c7b516
Antonstockmarr <43210147+Antonstockmarr@users.noreply.github.com>
2022-01-08 03:39:09 -0800

    Removing version check #patch
    All users are assumed to meet the version requirement in this check, and it produces an error if the version is not defined.
    Resolves #127

--- src/ScaleUnitManagement/WorkloadSetupOrchestrator/ScaleUnitWorkloadInstaller.cs
+++ src/ScaleUnitManagement/WorkloadSetupOrchestrator/ScaleUnitWorkloadInstaller.cs
@@ -66,10 +66,7 @@ namespace ScaleUnitManagement.WorkloadSetupOrchestrator
                         ClearPotentiallyProblematicTables();
                     }
                 }
-
-                // Assuming that the LBD environment will be on the app version >= 10.0.17
-                if (scaleUnit.EnvironmentType == EnvironmentType.LBD || AxDeployment.IsApplicationVersionMoreRecentThan("10.8.581.0"))
-                    await WaitForWorkloadInstallation(workloadInstance);
+                await WaitForWorkloadInstallation(workloadInstance);
             }
         }
 
