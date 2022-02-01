9b0d241c0f2417005c918930fdc6073d95303c49
Antonstockmarr <43210147+Antonstockmarr@users.noreply.github.com>
2022-02-01 07:45:52 -0800

    Create Action for running initialization steps
    Separate the logic of creating and running steps from the EnableScaleUnitFeature menu, making it simpler and more decoupled.

--- src/CLI/Menus/EnableScaleUnitFeature.cs
+++ src/CLI/Menus/EnableScaleUnitFeature.cs
@@ -1,8 +1,8 @@
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using CLI.Actions;
+using CLI.Utilities;
 using CLIFramework;
-using ScaleUnitManagement.ScaleUnitFeatureManager.Common;
 using ScaleUnitManagement.ScaleUnitFeatureManager.Utilities;
 using ScaleUnitManagement.Utilities;
 
@@ -11,43 +11,26 @@ namespace CLI.Menus
     internal class EnableScaleUnitFeature : DevToolMenu
     {
         protected List<IStep> availableSteps;
+        private ScaleUnitInstance scaleUnit;
 
         public override async Task Show(int input, string selectionHistory)
         {
-            List<CLIOption> options = SelectScaleUnitOptions(GetSortedScaleUnits(), PrintAvailableStepsForScaleUnit);
+            List<CLIOption> options = SelectScaleUnitOptions(GetSortedScaleUnits(), PrintAvailableSteps);
             var screen = new SingleSelectScreen(options, selectionHistory, "Environments:\n", "\nWhich environment would you like to configure?: ");
             await CLIController.ShowScreen(screen);
         }
 
-        private async Task PrintAvailableStepsForScaleUnit(int input, string selectionHistory)
+        private async Task PrintAvailableSteps(int input, string selectionHistory)
         {
-            string scaleUnitId = GetSortedScaleUnits()[input - 1].ScaleUnitId;
-            using var context = ScaleUnitContext.CreateContext(scaleUnitId);
-            if (ScaleUnitContext.GetScaleUnitId().Equals("@@"))
-                await new EnableScaleUnitFeatureOnHub().PrintAvailableSteps(input, selectionHistory);
-            else
-                await new EnableScaleUnitFeatureOnScaleUnit().PrintAvailableSteps(input, selectionHistory);
-        }
-
-        protected virtual List<IStep> GetAvailableSteps()
-        {
-            var sf = new StepFactory();
-            List<IStep> steps = sf.GetStepsOfType<ICommonStep>();
-            return steps;
-        }
-
-        protected async Task PrintAvailableSteps(int input, string selectionHistory)
-        {
+            scaleUnit = GetSortedScaleUnits()[input - 1];
             var options = new List<CLIOption>();
-            availableSteps = GetAvailableSteps();
-            availableSteps.Sort((x, y) => x.Priority().CompareTo(y.Priority()));
+            var stepGenerator = new StepGenerator(scaleUnit.ScaleUnitId);
+            availableSteps = stepGenerator.GetSteps();
 
             foreach (IStep step in availableSteps)
             {
                 options.Add(new CLIOption() { Name = step.Label(), Command = RunStep });
             }
-
-            ScaleUnitInstance scaleUnit = Config.FindScaleUnitWithId(ScaleUnitContext.GetScaleUnitId());
 
             string helpMessage = $"Please select steps you would like to run by supplying the step numbers\n" +
                 $"in a comma separated list, e.g. \"1,2,5,6\", or skip specific steps by supplying the negative step numbers\n" +
@@ -61,15 +44,8 @@ namespace CLI.Menus
 
         private async Task RunStep(int input, string selectionHistory)
         {
-            try
-            {
-                Console.WriteLine("Executing step: " + availableSteps[input - 1].Label());
-                await availableSteps[input - 1].Run();
-            }
-            catch (Exception ex)
-            {
-                Console.Error.WriteLine($"Error occurred while enabling scale unit feature:\n{ex}");
-            }
+            var action = new StepAction(scaleUnit.ScaleUnitId, availableSteps[input - 1]);
+            await action.Execute();
         }
     }
 }
