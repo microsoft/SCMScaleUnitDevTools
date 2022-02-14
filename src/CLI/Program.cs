e2687af3b4ca39237816de0a5195346d4fd933be
Antonstockmarr <43210147+Antonstockmarr@users.noreply.github.com>
2022-02-14 05:46:40 -0800

    Enable multibox deployment script
    The user can type --hub-deploy or --spoke-deploy to run deployment for one of the scale units.

--- src/CLI/Program.cs
+++ src/CLI/Program.cs
@@ -26,32 +26,14 @@ namespace CLI
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex);
+                Console.WriteLine(ex.Message);
                 Console.WriteLine("\n" + argumentParser.HelpMessage());
                 return;
             }
 
-            var deployer = new Deployer();
+            var argumentHandler = new ArgumentHandler();
+            await argumentHandler.RunScripts(argumentParser);
 
-            if (argumentParser.CleanStorage)
-            {
-                await deployer.CleanStorage();
-            }
-
-            if (argumentParser.Deploy)
-            {
-                await deployer.Deploy();
-            }
-
-            if (argumentParser.DrainPipelines)
-            {
-                await deployer.DrainAllPipelines();
-            }
-
-            if (argumentParser.StartPipelines)
-            {
-                await deployer.StartAllPipelines();
-            }
         }
     }
 }
--- src/CLI/Utilities/Deployer.cs
+++ src/CLI/Utilities/Deployer.cs
@@ -7,99 +7,36 @@ using ScaleUnitManagement.Utilities;
 
 namespace CLI.Utilities
 {
-    internal class Deployer
+    internal abstract class Deployer
     {
-        private readonly List<ScaleUnitInstance> sortedScaleUnitInstances;
+        public abstract Task Deploy();
 
-        public Deployer()
+        protected async Task InitializeEnvironments(ScaleUnitInstance scaleUnit)
         {
-            sortedScaleUnitInstances = Config.ScaleUnitInstances();
-            sortedScaleUnitInstances.Sort();
-        }
-
-        public async Task Deploy()
-        {
-            if (!Config.UseSingleOneBox())
+            Console.WriteLine($"\nInitializing environment on {scaleUnit.PrintableName()}");
+            var stepGenerator = new StepGenerator(scaleUnit.ScaleUnitId);
+            List<IStep> steps = stepGenerator.GetSteps();
+            foreach (IStep step in steps)
             {
-                throw new Exception("You can only use automatic deployment in a SingleOneBox environment");
-            }
-
-            await InitializeEnvironments();
-            await ConfigureEnvironments();
-            await InstallWorkloads();
-
-            Console.WriteLine("\nHub and spoke environments have been deployed successfully!\n");
-        }
-
-        public async Task CleanStorage()
-        {
-            foreach (ScaleUnitInstance scaleUnit in sortedScaleUnitInstances)
-            {
-                Console.WriteLine($"\nCleaning environment on {scaleUnit.PrintableName()}");
-                var action = new CleanUpStorageAccountAction(scaleUnit.ScaleUnitId);
+                var action = new StepAction(scaleUnit.ScaleUnitId, step);
                 await action.Execute();
             }
+
+            Console.WriteLine($"\nAll initialization steps completed for {scaleUnit.PrintableName()}");
         }
 
-        public async Task DrainAllPipelines()
+        protected async Task ConfigureEnvironments(ScaleUnitInstance scaleUnit)
         {
-            Console.WriteLine($"\nDraining all data pipelines");
-            foreach (ScaleUnitInstance scaleUnit in Config.ScaleUnitInstances())
-            {
-                var action = new DrainPipelinesAction(scaleUnit.ScaleUnitId);
-                await action.Execute();
-            }
-            Console.WriteLine("Done.");
+            Console.WriteLine($"\nPreparing {scaleUnit.PrintableName()} for installation");
+            var action = new ConfigureEnvironmentAction(scaleUnit.ScaleUnitId);
+            await action.Execute();
         }
 
-        public async Task StartAllPipelines()
+        protected async Task InstallWorkloads(ScaleUnitInstance scaleUnit)
         {
-            Console.WriteLine($"\nStarting all data pipelines");
-            foreach (ScaleUnitInstance scaleUnit in Config.ScaleUnitInstances())
-            {
-                var action = new StartPipelinesAction(scaleUnit.ScaleUnitId);
-                await action.Execute();
-            }
-            Console.WriteLine("Done.");
-        }
-
-        private async Task InitializeEnvironments()
-        {
-            foreach (ScaleUnitInstance scaleUnit in sortedScaleUnitInstances)
-            {
-                Console.WriteLine($"\nInitializing environment on {scaleUnit.PrintableName()}");
-                var stepGenerator = new StepGenerator(scaleUnit.ScaleUnitId);
-                List<IStep> steps = stepGenerator.GetSteps();
-                foreach (IStep step in steps)
-                {
-                    var action = new StepAction(scaleUnit.ScaleUnitId, step);
-                    await action.Execute();
-                }
-
-                Console.WriteLine($"\nAll initialization steps completed for {scaleUnit.PrintableName()}");
-            }
-        }
-
-        private async Task ConfigureEnvironments()
-        {
-            foreach (ScaleUnitInstance scaleUnit in sortedScaleUnitInstances)
-            {
-                Console.WriteLine($"\nPreparing {scaleUnit.PrintableName()} for installation");
-                var action = new ConfigureEnvironmentAction(scaleUnit.ScaleUnitId);
-                await action.Execute();
-            }
-        }
-
-        private async Task InstallWorkloads()
-        {
-            foreach (ScaleUnitInstance scaleUnit in sortedScaleUnitInstances)
-            {
-                Console.WriteLine($"\nInstalling workloads on {scaleUnit.PrintableName()}");
-                var action = new InstallWorkloadsAction(scaleUnit.ScaleUnitId);
-                await action.Execute();
-            }
+            Console.WriteLine($"\nInstalling workloads on {scaleUnit.PrintableName()}");
+            var action = new InstallWorkloadsAction(scaleUnit.ScaleUnitId);
+            await action.Execute();
         }
     }
 }
-
-
